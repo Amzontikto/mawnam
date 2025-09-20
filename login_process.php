d46a636e039f9db84e379064734c758bbeef9102
Amzontikto <lohmaytineeguynopparat@gmail.com>
2025-09-20 19:50:18 +0700

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -1,26 +1,26 @@
 <?php
 session_start();
+include "db.php";
 
-// เชื่อมต่อฐานข้อมูล
-$conn = new mysqli("localhost", "root", "", "mydb");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// รับค่าจากฟอร์ม
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// ตรวจสอบกับ DB
+// ตรวจสอบว่ามี user นี้ใน DB ไหม
 $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $_SESSION['username'] = $username;
-    header("Location: home.php");
+
+    echo "<script>
+        localStorage.setItem('currentUser', '$username');
+        window.location.href = 'index.html';
+    </script>";
 } else {
-    echo "<script>alert('ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง'); window.history.back();</script>";
+    echo "<script>
+        alert('ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง');
+        window.location.href='login.html';
+    </script>";
 }
 $conn->close();
 ?>
